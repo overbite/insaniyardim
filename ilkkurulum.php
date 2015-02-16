4b1d09a4f1395a9be51705e4f01bba86f4985e72
overbite <overbite@users.noreply.github.com>
2015-02-16 22:58:38 +0200

    Create ilkkurulum.php

---
+++ ilkkurulum.php
@@ -0,0 +1,15 @@
+<?php
+require 'mysql.php';
+$connection = new Configdb;
+$sifre = "ŞİFREYİ BURAYA YAZIN";
+
+$sifrex = md5(md5($sifre));
+
+$ekle = $connection->query("INSERT INTO users(kadi,sifre) VALUES('$kadi','$sifrex') ");
+
+if ($ekle) {
+  echo "islem tamam, ilkkurulum.php dosyasini siliniz.";
+} else {
+  echo "hata: admin eklenemedi.";
+}
+?>
