a8f8c90dbc4595c0983a81a9e41505acf9169635
overbite <overbite@users.noreply.github.com>
2015-02-19 16:16:41 +0200

    Update ilkkurulum.php

--- ilkkurulum.php
+++ ilkkurulum.php
@@ -4,7 +4,7 @@ $connection = new Configdb;
 $sifre = "ŞİFREYİ BURAYA YAZIN";
 
 $sifrex = md5(md5($sifre));
-
+$kadi = "admin";
 $ekle = $connection->query("INSERT INTO users(kadi,sifre) VALUES('$kadi','$sifrex') ");
 
 if ($ekle) {
